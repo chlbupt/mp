0fa74bd007186ab9b4ec3e0247260b8e832ddd2a
chlbupt <1032326058@qq.com>
2021-08-19 16:59:27 +0800

    version conflict

--- src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -2,11 +2,15 @@ package com.atguigu.mybatisplus.mapper;
 
 import com.atguigu.mybatisplus.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
 import java.util.List;
 
 //@Repository
 public interface UserMapper extends BaseMapper<User> {
     List<User> selectAllByNameUsers(String name);
+
+    // 查询：根据年龄查询用户列表，分页显示
+    IPage<User> selectPageByAge(Page<User> page, Integer age);
 }
--- src/test/java/com/atguigu/mybatisplus/InterceptorTests.java
+++ src/test/java/com/atguigu/mybatisplus/InterceptorTests.java
@@ -1,6 +1,8 @@
 package com.atguigu.mybatisplus;
 
+import com.atguigu.mybatisplus.entity.Product;
 import com.atguigu.mybatisplus.entity.User;
+import com.atguigu.mybatisplus.mapper.ProductMapper;
 import com.atguigu.mybatisplus.mapper.UserMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,9 @@ import java.util.List;
 public class InterceptorTests {
     @Resource
     private UserMapper userMapper;
+
+    @Resource
+    private ProductMapper productMapper;
 
     @Test
     public void testSelectPage(){
@@ -29,5 +34,42 @@ public class InterceptorTests {
 
         boolean bp = userPage.hasPrevious();
         System.out.println("上一页?" + bp);
+    }
+
+    @Test
+    public void testSelectPageByAge(){
+        Page<User> pageParam = new Page<>(3, 5);
+        userMapper.selectPageByAge(pageParam, 50);
+
+        List<User> users = pageParam.getRecords();
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testConcurrentUpdate(){
+        // 小李取价格
+        Product p1 = productMapper.selectById(1L);
+        // 小王取价格
+        Product p2 = productMapper.selectById(1L);
+
+        // 小李 + 50
+        p1.setPrice(p1.getPrice() + 50);
+        int result1 = productMapper.updateById(p1);
+        System.out.println("小李修改结果：" + result1);
+        // 小王 - 30
+        p2.setPrice(p2.getPrice() - 30);
+        int result2 = productMapper.updateById(p2);
+        System.out.println("小王修改结果：" + result2);
+        // 小王重试
+        if(result2 == 0){
+            p2 = productMapper.selectById(1L);
+            p2.setPrice(p2.getPrice() - 30);
+            result2 = productMapper.updateById(p2);
+        }
+        System.out.println("小王重试修改的结果：" + result2);
+
+        // 老板看价格
+        final Product p3 = productMapper.selectById(1L);
+        System.out.println("最后的结果：" + p3.getPrice());
     }
 }
